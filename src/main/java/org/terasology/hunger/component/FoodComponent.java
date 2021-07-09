ea391ec8705a1e2d86a87b724258b3f6c9b7c61b
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:19 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/hunger/component/FoodComponent.java
+++ src/main/java/org/terasology/hunger/component/FoodComponent.java
@@ -1,30 +1,25 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.hunger.component;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.utilities.modifiable.ModifiableValue;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * This component uses the ModifiableValue type Any modifications to the data members of this component must be via
  * ModifiableValue modifiers ModifiableValue.getValue can be used to get the value of the data members
  */
-public class FoodComponent implements Component {
+public class FoodComponent implements Component<FoodComponent> {
     /**
      * The amount of hunger this food restores when used.
      */
     public ModifiableValue filling;
+
+    @Override
+    public void copy(FoodComponent other) {
+        ModifiableValue newFilling = new ModifiableValue(other.filling.getBaseValue());
+        newFilling.setMultiplier(other.filling.getMultiplier());
+        newFilling.setPreModifier(other.filling.getPreModifier());
+        newFilling.setPostModifier(other.filling.getPostModifier());
+    }
 }
